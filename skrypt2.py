dd56d3578163cac1cb8de7de2c4da2c781f4fd9d
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2020-11-14 08:37:57 +0100

    Update skrypt2.py
    male zmiany dotyczace funkcjonalnosci skryptu

--- skrypt2.py
+++ skrypt2.py
@@ -1,24 +1,32 @@
 imie = 'Ala'
-imie2 = 'Petronella'
+#imie2 = 'Petronella'
 zwierze = 'kot'
 zmienna = 'Pe'
-#print ("{0} ma {1}a".format(imie, zwierze))
+zwierze = input("podaj zwierze ")
+print ("{0} ma {1}a".format(imie, zwierze))
 
-ile = int(input("podaj ilsc kotow Ali: "))
-if ile == 1:
-    print("{0} ma {1} {2}a".format(imie, ile, zwierze))
-elif ile > 3 and zwierze == 'kot':
-    print("{0} jest straszan kociara. Ma az {1} {2}ow!".format(imie, ile, zwierze))
+#zabawa z funcja format oraz if-em
+# ile = int(input("podaj ilsc kotow Ali: "))
+# if ile == 1:
+#     print("{0} ma {1} {2}a".format(imie, ile, zwierze))
+# elif ile > 3 and zwierze == 'kot':
+#     print("{0} jest straszan kociara. Ma az {1} {2}ow!".format(imie, ile, zwierze))
+# else:
+#     print("{0} ma {1} {2}ow".format(imie, ile, zwierze))
+
+#moja metoda na sprawszenie czy wyraz zaczyna sie na Pe
+imie2= input("podaj imie")
+print(imie2[0:2])
+if imie2[0:2] == zmienna:
+   print("imie {0} zaczyna sie na Pe".format(imie2))
 else:
-    print("{0} ma {1} {2}ow".format(imie, ile, zwierze))
+    print("imie {0} nie zaczyna sie na Pe".format(imie2))
 
-#print(imie2[0:2])
-#if imie2[0:2] == zmienna:
-#    print("imie zaczyna sie na Pe")
-#else:
-#    print("imie nie zaczyna sie na Pe")
- if zwierze.startswith('ko'):
-     print("zwierze zaczyna sie na ko")
+#sprawdzenie czy wyraz zaczyna sie na ko
+if zwierze.startswith('ko'):
+    print("zwierze zaczyna sie na ko")
 
-if 'ot' in zwierze:
-    print("dziala")
+#sprawdzenie czy wyraz zawiera ot w nazwie
+zwierze_lower = zwierze.lower()
+if 'ot' in zwierze_lower:
+    print("Zwierze ma ot w nazwie")
