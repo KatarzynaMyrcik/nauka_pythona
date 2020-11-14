271bff9346a62b5bc2e08575c1166b8c69dd04b7
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2020-11-14 08:52:42 +0100

    Update skrytp3.py
    poprawki logiczne

--- skrytp3.py
+++ skrytp3.py
@@ -1,21 +1,29 @@
-samochody = ['syrena', 'poloneza']
+samochody = ['syrena', 'polonez']
 
-print(samochody[0])
-print(samochody[1])
+# print(samochody[0])
+# print(samochody[1])
 
-for s in samochody:
-    print("samochod to " + s)
+# for po dowolnej zmiennej przyjmujacej argunemnty listy
+#for dowolna_zmienna in samochody:
+#    print("samochod to " + dowolna_zmienna)
+#    print("egzekucja for-a")
 
-print("petla z idx:")
-for idx in [0,1]:
-    print(samochody[idx])
+#petla po wygenerowanym autoatycznie numerze indeksu idx
+# for idx in [0, 1]:
+#     print(samochody[idx], idx)
 
-print("petla numer trzy po indeksie")
-print("len:{0}".format(len(samochody)))
-print("range: {0}".format(range(4)))
+# petla numer trzy po numerze indeksu
+# print("len:{0}".format(len(samochody)))
+# print("range: {0}".format(range(4)))
 
-for idx in range (len(samochody)):#len(samochody wyswietla dlugosc tablicy)
-    print("{0} {1}".format(idx,samochody[idx]))
-#    print("idx: " + " : " + samochody[idx])
+for idx in range(len(samochody)):  # len(samochody wyswietla dlugosc tablicy)
+    print("index to {0}, a samochod to: {1}".format(idx, samochody[idx]))
+    print("idx: " + samochody[idx])
 
 #print(samochody[3])
+
+
+#wizualizacja range. O co walsciwie w tym chodzi
+# zmienna = range(5)
+# print(zmienna[0:2])
+# print(zmienna[2])
