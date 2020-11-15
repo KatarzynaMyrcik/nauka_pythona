3b8028015d197c2a31c3b2f81bdb1b04d3605a37
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2020-11-15 07:54:08 +0100

    Update skrytp4.py

--- skrytp4.py
+++ skrytp4.py
@@ -1,20 +1,19 @@
 zabawki = ['kucyki', 'lalki', 'klocki']
 
-print(zabawki[0])
-print(zabawki[1])
-print(zabawki[2])
+# print(zabawki[0])
+# print(zabawki[1])
+# print(zabawki[2])
 
-print("pierwsza petla:")
-for s in zabawki:
-    print("zabawki to " + s)
+# print("pierwsza petla po zmiennej:")
+# for s in zabawki:
+#     print("zabawki w pokoju to " + s)
 
-print("druga petla:")
-for idx in [0,1,2]:
-    print("zabawka to " + zabawki[idx])
+# print("druga petla po indeksie:")
+# for idx in [0, 1, 2]:
+#     print("zabawka to " + zabawki[idx])
 
 print("trzecia petla:")
 for idx in range(len(zabawki)):
     print("zabawka to takze: " + zabawki[idx])
 
-
-print(range(5))
+# print(range(5))
