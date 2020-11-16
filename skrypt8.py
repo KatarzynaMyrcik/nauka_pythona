8aaf838269d0f44a0114e659be86ce7ff453cae3
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2020-11-16 08:45:02 +0100

    Update skrypt8.py
    zmiany w celu wiekszej czytelnosci notatek

--- skrypt8.py
+++ skrypt8.py
@@ -1,50 +1,45 @@
-#produkty = {'S1222': 'sukienka trojkat',
+#przeszukiwanie slownika w petli
+# produkty = {'S1222': 'sukienka trojkat',
 #            'P1222':'spodnie krata',
 #            'X212': 'konsola do gier'}
-#print(produkty)
-#igla = 'X2X'
-#produkty['X2X'] = 'igla'
-#print(produkty)
-#if igla in produkty:
-#     print('istnieje')
-#     print(produkty['p12'])
-
-#if igla in produkty:
-#    print("Znalazlem {0}".format(igla))
-#else:
-#    print("Brak w magazynie {0}".format(igla))
+# print(produkty)
+# igla = 'X2X'
+# produkty['X2X'] = 'igla'
+# print(produkty)
+# if igla in produkty:
+#     print('przedmiot o numerze {} istnieje w zbiorze'.format(igla))
+#
+# if igla in produkty:
+# #    print(igla)
+#     print("Znalazlem {0}".format(produkty['X2X']))
+# else:
+#     print("Brak w magazynie {0}".format(produkty['X2X']))
 #
 
 produkty = {
-            'SS12': {'nazwa': 'sukienka_trojka', 'rozmiary': ['s','l','xl']},
+            'SS12': {'nazwa': 'sukienka_trojkat', 'rozmiary': ['s','l','xl']},
             'P12' : {'nazwa': 'spodnie', 'rozmiary': ['s','xl']}
             }
-#for p in products:
-#    print(produkty)
 
+# for p in produkty:
+#     print(produkty)
+#     print(p)
+#
 for id in produkty:
     p = produkty[id]
+#    print(id)
 #    print(p)
     print(p['nazwa'])
+#    print(p['rozmiary'])
     rozmiary = p['rozmiary']
-    print(rozmiary)
-#    for r in rozmiary:
-#        print(r)
-
-#    print(produkty)
+    for r in rozmiary:
+        print(r)
+#print(produkty)
 
 
-#WyNiK Ma BYC Taki:
-#sukienka_trojka
-#s
-#m
-#l
-#spodnie
-#s
-#l
-
-#lista w ktorej kazda linia to slownik
-#koszyk = [
-#            {'nazwa':'mleko', 'cena':'5.3'}
-#            {'nazwa':'kaszanka', ''cena':'4'}
-#]
+# lista w ktorej kazda linia to slownik
+# koszyk = [
+#            {'nazwa':'mleko', 'cena':'5.3'},
+#            {'nazwa':'kaszanka', 'cena':'4'}
+# ]
+# print(koszyk)
