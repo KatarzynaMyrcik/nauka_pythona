4bd2844ec207614b7a4a3f8db4ffccda018d4422
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2020-11-16 08:54:13 +0100

    Update skrypt8_1.py
    dodanie petli

--- skrypt8_1.py
+++ skrypt8_1.py
@@ -1,9 +1,16 @@
 kucyki = {
-        'cynamonka': {'kolor':'szary', 'rodzaj':'latajacy', 'obecnosc_skrzydel':'tak', 'obecnosc_rogu':'nie'},
-        'Rarity' : {'kolor':'fioletowy', 'rodzaj':'naziemny', 'obecnosc_skrzydel':'nie', 'obecnosc_rogu':'tak'},
-        'RainbowDash': {'kolor':'niebieski','rodzaj':'latajacy', 'obecnosc_skrzydel':'tak', 'obecnosc_rogu':'nie'},
-        'AppleJack': {'kolor':'pomaranczowy','rodzaj':'latajacy', 'obecnosc_skrzydel':'tak','obecnosc_rogu':'nie'}
+        'cynamonka': {'kolor':'szary', 'rodzaj':'latajacy', 'obecnosc_skrzydel':'tak', 'obecnosc_rogu':'nie', 'moce':['milosc','dobro']},
+        'Rarity' : {'kolor':'fioletowy', 'rodzaj':'naziemny', 'obecnosc_skrzydel':'nie', 'obecnosc_rogu':'tak', 'moce':['wdziek','gust']},
+        'RainbowDash': {'kolor':'niebieski','rodzaj':'latajacy', 'obecnosc_skrzydel':'tak', 'obecnosc_rogu':'nie', 'moce':['szybkosc','sprawiedliwosc']},
+        'AppleJack': {'kolor':'pomaranczowy','rodzaj':'latajacy', 'obecnosc_skrzydel':'tak','obecnosc_rogu':'nie', 'moce':['pracowitosc','przyjacielskosc','sila']}
         }
 print(kucyki)
-#for id in kucyki:
-#    print(id)
+
+for id in kucyki:
+   print(id)
+   cechy_kucykow = kucyki[id]
+#   print(cechy_kucykow)
+#    print(cechy_kucykow['moce'])
+   moc = cechy_kucykow['moce']
+   for m in moc:
+       print(m)
