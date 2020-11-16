1ea6905e22ac9e56748356db702a8fe5cb9fe26e
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2020-11-16 07:43:45 +0100

    Update skrypt7.py

--- skrypt7.py
+++ skrypt7.py
@@ -1,21 +1,21 @@
+#cwiczenia ze slownikiem
 samolot = {'name': 'boeing',
             'przebieg': 10000,
             'type': 'pasazerski'}
-print(samolot)
-#print(samolot['name'])
-#print(samolot['type'])
-print(samolot.get('name'))
+# print(samolot)
+# print(samolot['name'])
+# print(samolot['type'])
+# print(samolot.get('name'))
+#dodanie zmiennej do slownika
 #print(samolot['nana'])
-#print(samolot.get('nanan'))
+print(samolot.get('nanan'))
 samolot['silnik'] = 'odrzutowy'
+print(samolot)
 
-#petla po kluczu - key
-for klucz in samolot:
-    print("{0}:{1}".format(klucz, samolot[klucz]))
+# #petla po kluczu - key
+# for klucz in samolot:
+#     print("{0}:{1}".format(klucz, samolot[klucz]))
 
 #petla po kluczu i wartosci
 for key, value in samolot.items():
     print("{0}:{1}".format(key, value))
-
-#for key in samolot:
-#    print("{0}:{1}".format(key,samolot[key]))
