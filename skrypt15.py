aad6e0e154a34e57ecc568c94c43787d10d9f84a
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2020-11-16 08:59:12 +0100

    Update skrypt15.py

--- skrypt15.py
+++ skrypt15.py
@@ -3,9 +3,13 @@ def print_dict(dict):
         print("{0}:{1}".format(key, dict[key]))
 
 
-if __name__ == '__main__':
-    print('tu jestem')
-    samolot = {'n':'mig','przeb':1220}
-    print(samolot)
-    print(type(samolot))
-    print_dict(samolot)
+# if __name__ == '__main__':
+#     print('tu jestem')
+
+samolot = {'n':'mig','przebieg':1220}
+kuchnia = {'lodowka':'samsung','piec':'amica','garnki':'gerlach'
+           
+print(samolot)
+print(type(samolot))
+print_dict(samolot)
+print_dict(kuchnia)
